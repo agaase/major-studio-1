6e5dac5d71be203d94469fa6af197fdc523cbe45
aseem agarwal <aseem@newschool.edu>
2016-09-08 18:48:26 +0000

    updating exercises

--- exercises/data-load/js/data-load.js
+++ exercises/data-load/js/data-load.js
@@ -1,16 +1,42 @@
-var table;
-
-function preload() {
-  //my table is comma separated value "csv"
-  //and has a header specifying the columns labels
-  table = loadTable("groceries.tsv", "tsv", "header");
-  //the file can be remote
-  //table = loadTable("http://p5js.org/reference/assets/mammals.csv",
-  //                  "csv", "header");
-}
 function setup() {
-  //count the columns
-  println(table.getRowCount() + " total rows in table");
-  println(table.getColumnCount() + " total columns in table");
+  
+  createCanvas(windowWidth,windowHeight);
+  loadTSV();
+  showJSON();
 
+}
+
+var loadTSV = function(){
+  loadTable("groceries.tsv","tsv","header",function(data){
+      var count = data.getRowCount();
+      var rowHeight = height/count;
+      
+      for(var i=0;i<count;i++){
+        var amount = data.getString(i,0);
+        var unit = data.getString(i,1);
+        var item = data.getString(i,2);
+        var source = data.getString(i,3);
+        
+        if(source === "market"){
+            fill("#BBDEFB");
+        }else{
+            fill("#C0CA33");
+        }
+        textSize(32);
+        text(amount + ' ' + unit + ' ' + item + " - " + source,100, 100 + rowHeight * (i+1));
+        
+      }
+  })
+};
+
+var showJSON = function(){
+  loadJSON("colors.json",function(colors){
+    var posx = 125;
+    for(var color in colors){
+      fill(color);
+      noStroke();
+      ellipse(posx, 100,50,50);
+      posx += 60;
+    }
+  });
 }
