cde8257edd42b8633046e75410ace3d51634201f
ASEEM AGGARWAL <aseemaggarwal@ASEEMs-MBP-2.wireless.newschool.edu>
2016-09-15 12:14:34 -0400

    updating local work

--- exercises/data-load/js/data-load.js
+++ exercises/data-load/js/data-load.js
@@ -1,5 +1,5 @@
 function setup() {
-  
+
   createCanvas(windowWidth,windowHeight);
   loadTSV();
   showJSON();
@@ -10,13 +10,13 @@ var loadTSV = function(){
   loadTable("groceries.tsv","tsv","header",function(data){
       var count = data.getRowCount();
       var rowHeight = height/count;
-      
+
       for(var i=0;i<count;i++){
         var amount = data.getString(i,0);
         var unit = data.getString(i,1);
         var item = data.getString(i,2);
         var source = data.getString(i,3);
-        
+
         if(source === "market"){
             fill("#BBDEFB");
         }else{
@@ -24,7 +24,7 @@ var loadTSV = function(){
         }
         textSize(32);
         text(amount + ' ' + unit + ' ' + item + " - " + source,100, 100 + rowHeight * (i+1));
-        
+
       }
   })
 };
