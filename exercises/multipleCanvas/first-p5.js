cb822cf3c59b7a42aabd18f7c6873c62e8e4b02d
aseem agarwal <aseem@newschool.edu>
2016-10-27 18:13:02 +0000

    updating

--- exercises/multipleCanvas/first-p5.js
+++ exercises/multipleCanvas/first-p5.js
@@ -1,41 +1,66 @@
+var c = [];
+
 function setup(){
-  loadTable("LaborInNonAgricultSector.tsv","tsv","header",showData);
+  loadTable("LaborInNonAgricultSector.txt","tsv","header",showData);
 }
 
+ var minn = Number.MAX_VALUE;
+var maxx = 0;
 
 function showData(data){
+ 
   var count = data.getRowCount();
   for(var row=0;row<count; row++){
     for(var col=3;col<data.getColumnCount();col++){
-      var value = data.getFloat(row,col);
-      console.log(val)
+      var val = data.getString(row,col);
+      if(val){
+        val = parseFloat(val); 
+        if(val < minn){
+          minn = val;
+        }
+        if(val > maxx){
+          maxn = val;
+        }  
+      }
+      //console.log(value);
+    }
+  }
+  
+  for(var row=0;row<count; row++){
+    c[row] = newCanvas(data,row);
+    for(var col=3;col<data.getColumnCount();col++){
     }
   }
 }
 
-var c1 = function(p){
 
-  p.setup = function(){
-    p.createCanvas(400,300);
+function newCanvas(data, row){
+  debugger;
+  var c = function(p){
+    var width=400;
+    var height = 300;
+    p.setup = function(){
+      p.createCanvas(400,300);
+      p.rect(0,0,width-1,height-1);
+      p.text(data.getString(row,0),10,20);
+      p.beginShape();
+      for(var col=3;col<data.getColumnCount();col++){
+        var value = data.getString(row,col);
+        value = value ? parseFloat(value) : 0;
+        p.vertex(map(col,3,25,0,width),map(value,minn,maxx,0,height));
+      }
+      p.endShape(CLOSE);
+    }
+  
+    p.draw = function(){
+      // p.background("rgb(113, 178, 201)");
+      // p.ellipse(canvas1.mouseX, canvas2.mouseY, 30, 30);
+    }
   }
+  
+  
+  var myCanvas = new p5(c);
+  
+  return myCanvas;
 
-  p.draw = function(){
-    p.background("rgb(199, 228, 165)");
-    p.ellipse(canvas2.mouseX, canvas2.mouseY, 30, 30);
-  }
 }
-
-var c2 = function(p){
-
-  p.setup = function(){
-    p.createCanvas(400,300);
-  }
-
-  p.draw = function(){
-    p.background("rgb(113, 178, 201)");
-    p.ellipse(canvas1.mouseX, canvas2.mouseY, 30, 30);
-  }
-}
-
-var canvas1 = new p5(c1);
-var canvas2 = new p5(c2);
